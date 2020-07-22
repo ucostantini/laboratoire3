636d6f31bf2e1dd10c5ccbb72f192349e81c9210
abder123alg <46766202+abder123alg@users.noreply.github.com>
2020-07-22 19:18:06 -0400

    Update ImageModel.java

--- lab3/src/modele/ImageModel.java
+++ lab3/src/modele/ImageModel.java
@@ -4,13 +4,11 @@ package modele;
 import java.awt.*;
 
 public class ImageModel {
-    private String imageChemin;
     private Image image;
     private boolean isImageReady = false;
 
 
     public void setImage(String chemin){
-        imageChemin = chemin;
         image = Toolkit.getDefaultToolkit().getImage(chemin);
         isImageReady = true;
     }
