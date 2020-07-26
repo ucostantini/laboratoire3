62efd8a01ce1cd371792b977e10c93f6de5ca506
abder123alg <46766202+abder123alg@users.noreply.github.com>
2020-07-25 20:41:28 -0400

    Update Vignette.java

--- src/vues/Vignette.java
+++ src/vues/Vignette.java
@@ -5,33 +5,23 @@ import modeles.ModeleImage;
 import javax.swing.*;
 import java.awt.*;
 
-public class Vignette extends JPanel implements Observateur {
+public class Vignette extends JPanel {
 
-   private ModeleImage modeleImage;
+   private final ModeleImage modeleImage;
+
 
 
     public Vignette(ModeleImage image)
     {
         this.modeleImage = image;
 
-        //this.setLayout(null);
-        //this.setBounds(1000,70,150,200);
-        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
     }
-    public void setImage(ModeleImage modeleImage){
-        this.modeleImage = modeleImage;
-    }
+
 
     public void paintComponent (Graphics g){
         super.paintComponent(g);
-        if(modeleImage.getImage() != null){
-            g.drawImage(modeleImage.getImage().getScaledInstance(150, 200, Image.SCALE_DEFAULT),0,0,this);
-        }
-    }
-
-    @Override
-    public void update()
-    {
-        repaint();
+        g.drawImage(modeleImage.getImage(),0,0,
+                (int) (modeleImage.getImage().getWidth(null)* 0.2),
+                (int) (modeleImage.getImage().getHeight(null)* 0.2),null);
     }
 }
