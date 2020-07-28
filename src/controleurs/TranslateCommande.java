6f4b2620a730aecb8de5f21927175c71bb10e485
abder123alg <46766202+abder123alg@users.noreply.github.com>
2020-07-28 08:55:30 -0400

    Update TranslateCommande.java

--- src/controleurs/TranslateCommande.java
+++ src/controleurs/TranslateCommande.java
@@ -1,6 +1,10 @@
 package controleurs;
 
+import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.geom.NoninvertibleTransformException;
+import java.awt.geom.Point2D;
+
 import modeles.ModelePerspective;
 
 public class TranslateCommande implements Commande {
@@ -10,4 +14,28 @@ public class TranslateCommande implements Commande {
   public void execute(ModelePerspective mp, MouseEvent e) {
     mp.translate(e);
   }
+
+
+  @Override
+  public void undo(ModelePerspective mp) {
+    try {
+      int sizelist = mp.listPosition.size();
+
+      int index = sizelist - 1;
+      int index2 = sizelist - 2;
+
+      Point derElmList = mp.listPosition.get(index);
+      Point2D.Float dragStart = mp.transformPoint(mp.listPosition.get(index2));
+      Point2D.Float dragEnd = mp.transformPoint(derElmList);
+      mp.listPosition.remove(derElmList);
+      double dx = dragStart.getX() - dragEnd.getX();
+      double dy = dragStart.getY() - dragEnd.getY();
+      mp.coordTransform.translate(dx, dy);
+      mp.notifyObservers();
+    }catch (NoninvertibleTransformException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+
 }
