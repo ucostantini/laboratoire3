90de670b6fe3a22c77d322529badddfaa498df2b
abder123alg <46766202+abder123alg@users.noreply.github.com>
2020-07-28 08:56:39 -0400

    Update Commande.java

--- src/controleurs/Commande.java
+++ src/controleurs/Commande.java
@@ -4,7 +4,7 @@ import java.awt.event.MouseEvent;
 import modeles.ModelePerspective;
 
 public interface Commande {
-
   void execute(ModelePerspective mp, MouseEvent e);
-
+  void undo(ModelePerspective mp);
 }
+
