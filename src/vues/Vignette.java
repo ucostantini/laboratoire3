9fccdb3556810401b51c6df30baca711e1b72513
Catarina878 <60159728+Catarina878@users.noreply.github.com>
2020-08-03 12:24:19 -0400

    Update Vignette.java

--- src/vues/Vignette.java
+++ src/vues/Vignette.java
@@ -3,17 +3,24 @@ package vues;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 import modeles.ModeleImage;
-
+/**
+ * Classe qui sert a afficher la vignette
+ */
 public class Vignette extends JPanel {
 
   private final ModeleImage modeleImage;
-
+/**
+	 * Contructeur de l'image a afficher
+	 * @param image, image a afficher
+	 */
   public Vignette(ModeleImage image) {
     this.modeleImage = image;
 
   }
 
-
+	/**
+	 * Cette methode qui s'occupe de l'affichage
+	 */
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     g.drawImage(modeleImage.getImage(), 0, 0,
